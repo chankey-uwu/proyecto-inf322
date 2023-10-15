e744cb0e06a908e85ad291bb687017990acbe935
chankey-uwu <carlos.kuhn@usm.cl>
2023-10-15 19:39:51 -0300

    black buttons

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,18 +4,43 @@ import NavBar from '../components/nav_bar'
 
 export const HomePage = () => {
   const divStyle = {
-    width: '100%',
-    height: '70px',
+    maxWidth: '100%',
+    maxHeight: '75px',
+    minWidth: '100%',
+    minHeight: '75px',
+  };
+  const buttonStyle = {
+    maxWidth: '100%',
+    maxHeight: '50px',
+    minWidth: '100%',
+    minHeight: '50px',
+    backgroundColor: 'black',
   };
   return (
     <div className='page'>
       <h1 className='page__title'>Frontend Sample App</h1>
       <NavBar />
       <h2>Página de Inicio</h2>
-      <div style={divStyle}><Button style={{maxWidth: '100%', maxHeight: '50px', minWidth: '100%', minHeight: '50px'}} variant="contained" size="large">Reservar Quincho</Button></div> 
-      <div style={divStyle}><Button style={{maxWidth: '100%', maxHeight: '50px', minWidth: '100%', minHeight: '50px'}} variant="contained" size="large">Registrar Visitas</Button></div>
-      <div style={divStyle}><Button style={{maxWidth: '100%', maxHeight: '50px', minWidth: '100%', minHeight: '50px'}} variant="contained" size="large">Custodia Paquetes</Button></div>
-      <div style={divStyle}><Button style={{maxWidth: '100%', maxHeight: '50px', minWidth: '100%', minHeight: '50px'}} variant="contained" size="large" disabled disableRipple>Chat Vecinos</Button></div>
+      <div style={divStyle}>
+        <Button variant="contained" size="large" style={buttonStyle}>
+          Reservar Quincho
+        </Button>
+      </div>
+      <div style={divStyle}>
+        <Button variant="contained" size="large" style={buttonStyle}>
+          Registrar Visitas
+        </Button>
+      </div>
+      <div style={divStyle}>
+        <Button variant="contained" size="large" style={buttonStyle}>
+          Custodia Paquetes
+        </Button>
+      </div>
+      <div style={divStyle}>
+        <Button variant="contained" size="large" style={{maxWidth: '100%', maxHeight: '50px', minWidth: '100%', minHeight: '50px'}} disabled disableRipple>
+          Chat Vecinos
+        </Button>
+      </div>
     </div>
   )
 }
